01702bb1a64cd7a52ddb6e90f748b72e7773d362
PhoebePinney <phoebepinney@gmail.com>
2022-10-07 23:01:20 +0100

    Enable video play and word split

--- third-year-project/src/app/home/home.component.ts
+++ third-year-project/src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent{
+  word: string[] = [];
+  url = './assets/testVideo.mp4';
   constructor() { }
-
-  ngOnInit() {
+  onButton(word: string){
+    this.word = word.split('');
+    this.url = './assets/testVideo2.mp4'
+    console.log('yo');
   }
-
 }
