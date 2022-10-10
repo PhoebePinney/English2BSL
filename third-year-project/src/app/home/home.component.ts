578e777dfea362e98943c41256882911a503c890
PhoebePinney <phoebepinney@gmail.com>
2022-10-10 21:23:52 +0100

    Add input validation

--- third-year-project/src/app/home/home.component.ts
+++ third-year-project/src/app/home/home.component.ts
@@ -9,37 +9,53 @@ export class HomeComponent implements AfterViewInit{
   availableWords = ['Hello', 'my', 'name', 'is'];
   listOfWords: string[] = [];
   output: string[] = [];
+  message = '';
 
-  listOfVideos = ['./assets/testVideo2.mp4', './assets/testVideo.mp4'];
+  listOfVideos = ['./assets/videos/testVideo2.mp4', './assets/videos/testVideo.mp4'];
   i = 0;
   @ViewChild('videoPlayer') videoPlayer!: ElementRef;
+  @ViewChild('vidDiv') vidDiv!: ElementRef;
 
   constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit(): void {
-    this.playVid(0);
+    //this.playVid(0, false);
   }
 
   onButton(userInput: string){
-    this.listOfWords = userInput.split(' ');
-    var out: string[] = [];
-    for (const w in this.listOfWords){
-      if (this.availableWords.includes(this.listOfWords[w])){
-        out.push(this.listOfWords[w]);
-      }
-      else{
-        const splitWord = this.listOfWords[w].split('');
-        for (const l in splitWord){
-          out.push(splitWord[l]);
+    const checkInput = new RegExp(/[^a-zA-Z0-9\s\.]/);
+    if (!checkInput.test(userInput)){
+      this.message = '';
+      this.listOfWords = userInput.split(' ');
+      var out: string[] = [];
+      for (const w in this.listOfWords){
+        if (this.availableWords.includes(this.listOfWords[w])){
+          out.push(this.listOfWords[w]);
+        }
+        else{
+          const splitWord = this.listOfWords[w].split('');
+          for (const l in splitWord){
+            out.push(splitWord[l]);
+          }
         }
       }
+      this.output = out;
+      this.vidDiv.nativeElement.setAttribute("style", "display:block;")
+      this.playVid(0, true);
+    } else{
+      this.message = 'Invalid input';
+      this.output = [];
+      this.vidDiv.nativeElement.setAttribute("style", "display:none;")
     }
-    this.output = out;
   }
 
-  playVid(videoNum: number) {
+  playVid(videoNum: number, auto: Boolean) {
     this.videoPlayer.nativeElement.setAttribute("src", this.listOfVideos[videoNum]);
-    this.videoPlayer.nativeElement.autoplay = true;
+    if (auto){
+      this.videoPlayer.nativeElement.autoplay = true;
+    } else {
+      this.videoPlayer.nativeElement.autoplay = false;
+    }
     this.videoPlayer.nativeElement.load();
   }
 
@@ -47,9 +63,9 @@ export class HomeComponent implements AfterViewInit{
     this.i++;
     if (this.i == this.listOfVideos.length) {
         this.i = 0;
-        this.playVid(this.i);
+        this.playVid(this.i, false);
     } else {
-        this.playVid(this.i);
+        this.playVid(this.i, true);
     }
   }
 }
